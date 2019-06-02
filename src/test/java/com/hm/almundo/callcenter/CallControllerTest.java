efd9c1def8e231865c1ccca757ccbf66d405cbf1
Henry Martinez <hmartinez@novopayment.com>
2019-06-02 17:44:48 -0500

    Unit Test

--- src/test/java/com/hm/almundo/callcenter/CallControllerTest.java
+++ src/test/java/com/hm/almundo/callcenter/CallControllerTest.java
@@ -6,6 +6,7 @@ import com.hm.almundo.callcenter.controller.CallController;
 import com.hm.almundo.callcenter.controller.Dispatcher;
 import com.hm.almundo.callcenter.model.Call;
 import com.hm.almundo.callcenter.model.Employee;
+import com.hm.almundo.callcenter.util.Utils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,6 +18,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 @RunWith(SpringRunner.class)
 @WebFluxTest({CallController.class, Dispatcher.class, AppConfig.class})
 public class CallControllerTest {
@@ -26,22 +32,48 @@ public class CallControllerTest {
     private WebTestClient client;
 
     private Call call;
-    private Employee employee;
+    private Queue<Employee> employees;
+    private final int MAX_EMPLOYEE = 10;
 
     @Before
     public void createEmployeeDefault(){
-        employee = new Employee();
-        employee.setName("HM");
-        employee.setRole(Constants.OPERATOR);
+        employees = new LinkedList<>();
+        for (int i = 0; i < MAX_EMPLOYEE; i++) {
+            Employee employee = new Employee();
+            employee.setName("HM " + 1);
+            employee.setRole(getRandomRole());
+            employees.add(employee);
+        }
+    }
+
+    public String getRandomRole() {
+
+        int random = Utils.getRandomBeetwen(0,2);
+        String role = "";
+        switch (random){
+            case 0:
+                role = Constants.OPERATOR;
+                break;
+            case 1:
+                role = Constants.SUPERVISOR;
+                break;
+            case 2:
+                role = Constants.DIRECTOR;
+                break;
+
+        }
+
+        return role;
+
     }
 
 
     @Test
     public void assignEmployeeTest() throws Exception {
 
-        client.post().uri("/api/customer/post")
+        client.post().uri("/employee")
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromObject(employee))
+                .body(BodyInserters.fromObject(employees.remove()))
                 .exchange()
                 .expectStatus().isCreated()
                 .expectBody(Employee.class);
