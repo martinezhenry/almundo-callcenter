ce65b2b903155862bb60264e3313dc0684509f96
Henry Martinez <hmartinez@novopayment.com>
2019-06-02 18:27:27 -0500

    Unit Test

--- src/test/java/com/hm/almundo/callcenter/CallControllerTest.java
+++ src/test/java/com/hm/almundo/callcenter/CallControllerTest.java
@@ -7,6 +7,7 @@ import com.hm.almundo.callcenter.controller.Dispatcher;
 import com.hm.almundo.callcenter.model.Call;
 import com.hm.almundo.callcenter.model.Employee;
 import com.hm.almundo.callcenter.util.Utils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,14 +15,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.http.StreamingHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.reactive.function.BodyInserters;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
+import java.util.concurrent.*;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest({CallController.class, Dispatcher.class, AppConfig.class})
@@ -33,7 +33,7 @@ public class CallControllerTest {
 
     private Call call;
     private Queue<Employee> employees;
-    private final int MAX_EMPLOYEE = 10;
+    private final int MAX_EMPLOYEE = 12;
 
     @Before
     public void createEmployeeDefault(){
@@ -91,7 +91,72 @@ public class CallControllerTest {
 
     }
 
-    // POST Test-case
+    @Test
+    public void makeMultiCallsTest() throws Exception {
 
+        runMultiEmployee(10);
+        runMultiCalls(11);
+
+    }
+
+    private void runMultiEmployee(final int threadCount) throws InterruptedException, ExecutionException {
+        //final BrokenUniqueIdGenerator domainObject = new BrokenUniqueIdGenerator();
+        Callable<Employee> task = new Callable<Employee>() {
+            @Override
+            public Employee call() {
+                WebTestClient.ResponseSpec response = client.post().uri("/employee").accept(MediaType.APPLICATION_JSON).body(BodyInserters.fromObject(employees.remove())).exchange();
+                response = response.expectStatus().isCreated();
+                WebTestClient.BodySpec bodySpec= response.expectBody(Employee.class);
+                Employee employee = (Employee) bodySpec.returnResult().getResponseBody();
+
+                return employee;
+
+            }
+        };
+        List<Callable<Employee>> tasks = Collections.nCopies(threadCount, task);
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        List<Future<Employee>> futures = executorService.invokeAll(tasks);
+        List<Long> resultList = new ArrayList<Long>(futures.size());
+        // Check for exceptions
+        for (Future<Employee> future : futures) {
+            // Throws an exception if an exception was thrown by the task.
+            resultList.add(future.get().getCode());
+        }
+
+    }
+
+
+    private void runMultiCalls(final int threadCount) throws InterruptedException, ExecutionException {
+        //final BrokenUniqueIdGenerator domainObject = new BrokenUniqueIdGenerator();
+        Callable<Call> task = new Callable<Call>() {
+            @Override
+            public Call call() {
+                WebTestClient.ResponseSpec response = client.get().uri("/call/{id}", 2).accept(MediaType.APPLICATION_JSON).exchange();
+                response = response.expectStatus().isOk();
+                 WebTestClient.BodySpec bodySpec= response.expectBody(Call.class);
+                 Call call = (Call) bodySpec.returnResult().getResponseBody();
+
+                return call;
+
+            }
+        };
+        List<Callable<Call>> tasks = Collections.nCopies(threadCount, task);
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        List<Future<Call>> futures = executorService.invokeAll(tasks);
+        List<Long> resultList = new ArrayList<Long>(futures.size());
+        // Check for exceptions
+        for (Future<Call> future : futures) {
+            // Throws an exception if an exception was thrown by the task.
+            resultList.add(future.get().getId());
+        }
+        // Validate the IDs
+        Assert.assertEquals(threadCount, futures.size());
+        List<Long> expectedList = new ArrayList<Long>(threadCount);
+        for (long i = 1; i <= threadCount; i++) {
+            expectedList.add(i);
+        }
+        Collections.sort(resultList);
+        Assert.assertEquals(expectedList, resultList);
+    }
 
 }
